3d3e7280cc86745ba2b21b05711d6d0526d99e06
Rahil1910 <rahilrafique1234@gmail.com>
2024-12-30 21:29:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const sel = document.getElementById('colorSelect')
+const btn = document.getElementById('button')
+btn.addEventListener('click', function(){
+	const selIndex = sel.selectedIndex;
+	if(selIndex !== -1){
+		sel.remove(selIndex);
+	} 
+});
